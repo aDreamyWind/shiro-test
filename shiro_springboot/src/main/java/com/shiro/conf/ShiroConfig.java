64c0121337ddb6651ec50797ddff9fb78684c187
breeze <770457615@qq.com>
2023-02-21 22:45:30 +0800

    登出、记住我

--- shiro_springboot/src/main/java/com/shiro/conf/ShiroConfig.java
+++ shiro_springboot/src/main/java/com/shiro/conf/ShiroConfig.java
@@ -3,7 +3,9 @@ package com.shiro.conf;
 import com.shiro.realm.MyRealm;
 import org.apache.shiro.authc.credential.HashedCredentialsMatcher;
 import org.apache.shiro.spring.web.config.DefaultShiroFilterChainDefinition;
+import org.apache.shiro.web.mgt.CookieRememberMeManager;
 import org.apache.shiro.web.mgt.DefaultWebSecurityManager;
+import org.apache.shiro.web.servlet.SimpleCookie;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -31,8 +33,26 @@ public class ShiroConfig {
         myRealm.setCredentialsMatcher(macher);
         // 4、将myRealm存入到DefaultWebSecurityManager中
         defaultWebSecurityManager.setRealm(myRealm);
+        // 设置rememberMe
+        defaultWebSecurityManager.setRememberMeManager(rememberMeManager());
         // 5、返回
         return defaultWebSecurityManager;
+    }
+
+    public SimpleCookie rememberMeCookie(){
+        SimpleCookie cookie = new SimpleCookie("rememberMe");
+        // 跨域
+        cookie.setPath("/");
+        cookie.setHttpOnly(true);
+        cookie.setMaxAge(30*24*60*60);
+        return cookie;
+    }
+
+    public CookieRememberMeManager rememberMeManager() {
+        CookieRememberMeManager cookieRememberMeManager = new CookieRememberMeManager();
+        cookieRememberMeManager.setCookie(rememberMeCookie());
+        cookieRememberMeManager.setCipherKey("123456778908".getBytes());
+        return cookieRememberMeManager;
     }
 
     @Bean
@@ -41,8 +61,12 @@ public class ShiroConfig {
         // 设置不认证可以访问的资源
         definition.addPathDefinition("/myController/userLogin", "anon");
         definition.addPathDefinition("/myController/login", "anon");
+        // 登出过滤器
+        definition.addPathDefinition("/logout", "logout");
         // 设置要拦截的
         definition.addPathDefinition("/**", "authc");
+        // 存在用户的过滤器
+        definition.addPathDefinition("/**", "user");
         return definition;
     }
 }
--- shiro_springboot/src/main/java/com/shiro/contoller/MyController.java
+++ shiro_springboot/src/main/java/com/shiro/contoller/MyController.java
@@ -8,6 +8,7 @@ import org.apache.shiro.subject.Subject;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpSession;
 
@@ -21,16 +22,18 @@ import javax.servlet.http.HttpSession;
 public class MyController {
 
     @GetMapping("login")
-    public String login(){
+    public String login() {
         return "login";
     }
 
     @GetMapping("userLogin")
-    public String userLogin(String name, String pwd, HttpSession session) {
+    public String userLogin(String name, String pwd,
+                            @RequestParam(defaultValue = "false") boolean rememberMe,
+                            HttpSession session) {
         // 获取subject对象
         Subject subject = SecurityUtils.getSubject();
         // 封装请求数据到token
-        AuthenticationToken token = new UsernamePasswordToken(name, pwd);
+        AuthenticationToken token = new UsernamePasswordToken(name, pwd, rememberMe);
         // 调用login
         try {
             subject.login(token);
@@ -40,4 +43,19 @@ public class MyController {
             return "登陆失败";
         }
     }
+
+    @GetMapping("userLoginRm")
+    public String userLogin(HttpSession session) {
+        session.setAttribute("user", "rememberMe");
+        return "main";
+    }
+
+    // 登陆认证验证角色
+    // @RequiresRoles("admin")
+    // @GetMapping("userLoginRoles")
+    // @RequestBody
+    // public String userLoginRoles() {
+    //     System.out.println("登陆验证角色成功");
+    //     return "登陆验证角色成功";
+    // }
 }
